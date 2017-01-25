7b640705b991a7caf8dcd7bd12094502477ddcce
Valerie Boem <vboem@gmail.com>
2017-01-25 18:24:02 -0500

    Corrected the edit page to properly direct.

--- crm.rb
+++ crm.rb
@@ -28,7 +28,7 @@ end
 get '/contacts/:id/edit' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
-    erb :show_contact
+    erb :edit_contact
   else
     raise Sinatra::NotFound
   end
@@ -47,6 +47,20 @@ get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
     erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
   else
     raise Sinatra::NotFound
   end
