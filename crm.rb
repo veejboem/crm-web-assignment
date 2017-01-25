1e46ba0de6ede78f9e91a2cb6bd0b4601dc2d2f4
Valerie Boem <vboem@gmail.com>
2017-01-25 16:21:34 -0500

    Added show_contact.rb file & created new page on the crm app.

--- crm.rb
+++ crm.rb
@@ -5,9 +5,10 @@
 require_relative 'contact'
 require 'sinatra'
 
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+Contact.create('Betty', 'Maker', 'betty@bitmakerlabs.com', 'Developer')
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Valerie's CRM"
@@ -22,6 +23,11 @@ end
 
 get '/new_contact' do
   erb :new_contact
+end
+
+get '/contacts/1' do
+  @contact = Contact.find(1)
+  erb :show_contact
 end
 
 post '/contacts' do
