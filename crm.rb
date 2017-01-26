13aeebcea357d1ab9d6cff383d56f2482d7045f7
Valerie Boem <vboem@gmail.com>
2017-01-26 12:16:37 -0500

    Changed put '/contacts/:id' in crm.rb.

--- crm.rb
+++ crm.rb
@@ -58,14 +58,13 @@ get '/contacts/:id' do
 end
 
 put '/contacts/:id' do
-  @contact = Contact.find(params[:id].to_i)
-  if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
-
-    redirect to('/contacts')
+  if contact = Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
   else
     raise Sinatra::NotFound
   end
