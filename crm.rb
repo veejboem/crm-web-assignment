ac2c58208a6e8d59446aa2bf3f27a61e9244f084
Valerie Boem <vboem@gmail.com>
2017-01-25 20:01:35 -0500

    Added link on the show contact page to direct to the edit page.

--- crm.rb
+++ crm.rb
@@ -75,3 +75,21 @@ delete '/contacts/:id' do
     raise Sinatra::NotFound
   end
 end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    redirect to('/contacts/:id/edit')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+# put '/contacts/:id/edit' do
+#   @contact = Contact.find(params[:id].to_i)
+#   if @contact
+#     erb :edit_contact
+#   else
+#     raise Sinatra::NotFound
+#   end
+# end
